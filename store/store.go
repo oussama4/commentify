320a2d92833e9c1bd618e01166052672e88e7f3b
osama zowaki <oussamazouaki4@gmail.com>
2021-02-14 16:47:59 +0100

    change store interface

--- store/store.go
+++ store/store.go
@@ -4,11 +4,12 @@ import "github.com/oussama4/commentify/model"
 
 type Store interface {
 	GetComment(id string) (*model.Comment, error)
-	CreateComment(comment model.Comment) (*model.Comment, error)
-	UpdateComment(id string, body string) (*model.Comment, error)
+	CreateComment(body, parentId, userId, threadId string) (string, error)
+	UpdateComment(id string, body string) error
 	DeleteComment(id string) error
-	ListComments(threadUrl string) ([]*model.Comment, error)
+	ListComments(threadId string) ([]*model.CommentOutput, error)
+	ListChildComments(threadId, parentId string) ([]*model.CommentOutput, error)
 	GetThread(id string) (*model.Thread, error)
-	CreateThread(thread model.Thread) (*model.Thread, error)
-	CreateUser(user model.User) (*model.User, error)
+	CreateThread(url, domain, title string) (string, error)
+	CreateUser(name, email string) (string, error)
 }
