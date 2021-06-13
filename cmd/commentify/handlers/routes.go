46375ccd081d082fa9af4090291382209e67f493
oussama4 <oussamazouaki4@gmail.com>
2021-06-13 13:50:28 +0100

    update deps

--- cmd/commentify/handlers/routes.go
+++ cmd/commentify/handlers/routes.go
@@ -3,8 +3,8 @@ package handlers
 import (
 	"net/http"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/oussama4/commentify/store"
 )
 
