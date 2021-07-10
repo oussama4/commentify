e0fd757ed725f91cd97d09ab80c562091c7480dc
oussama4 <oussamazouaki4@gmail.com>
2021-07-10 12:32:00 +0100

    add cors

--- cmd/commentify/handlers/routes.go
+++ cmd/commentify/handlers/routes.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/oussama4/commentify/store"
 )
 
@@ -13,6 +14,14 @@ func Routes(store store.Store, logger *log.Logger) http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "Content-Range", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	u := &User{
 		store:  store,
