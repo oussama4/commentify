123b7c3d46f417b6fcac9edeabbfb3b9c149eed5
osama zowaki <oussamazouaki4@gmail.com>
2022-01-08 19:05:31 +0100

    add button component

---
+++ frontend/src/components/atoms/Button.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let value: String
+</script>
+
+<button class="transition duration-300 border border-gray-200 text-gray-500 rounded-lg shadow-sm hover:shadow-md text-base text-center w-full h-full font-normal inline-block py-2"> 
+    {value}
+</button>
