3f1b5ec8a8152a7cbb488807820a916d193be488
osama zowaki <oussamazouaki4@gmail.com>
2022-01-09 17:56:11 +0100

    setup nix environment

---
+++ shell.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/32356ce11b8cc5cc421b68138ae8c730cc8ad4a2.tar.gz") {} }:
+
+pkgs.mkShell {
+    name = "commentify-env";
+    buildInputs = [
+        pkgs.nodejs
+        pkgs.go_1_17
+        pkgs.yarn
+    ];
+}
