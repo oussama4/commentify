c3e219130f65d952209eccaae5af0fbfad070afe
osama zowaki <oussamazouaki4@gmail.com>
2022-01-09 21:04:28 +0100

    add postgresql to nix shell

--- shell.nix
+++ shell.nix
@@ -6,5 +6,24 @@ pkgs.mkShell {
         pkgs.nodejs
         pkgs.go_1_17
         pkgs.yarn
+        pkgs.go-task
+        pkgs.postgresql_14
     ];
+    shellHook = ''
+        export PGDATA=$HOME/postgres/data
+        export PGHOST=$PGDATA
+        export PGLOG=$HOME/postgres/postgres.log
+
+        mkdir -p $HOME/postgres
+
+        if [ ! -d $PGDATA ]; then
+            echo 'Initializing postgresql database...'
+            initdb -D $PGDATA --no-locale --encoding=UTF-8
+        fi
+
+        if ! pg_ctl status; then
+            echo 'starting postgresql database...'
+            pg_ctl start -l $PGLOG -o "--unix_socket_directories='$PGHOST'"
+        fi
+    '';
 }
