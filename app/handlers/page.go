225a9bb0e9dfe924a41a063b803ee5c7b88ebd33
osama zowaki <oussamazouaki4@gmail.com>
2022-02-05 20:31:48 +0100

    validate page input

--- app/handlers/page.go
+++ app/handlers/page.go
@@ -7,8 +7,9 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/oussama4/commentify/base/validate"
 	"github.com/oussama4/commentify/base/web"
-	"github.com/oussama4/commentify/store"
+	"github.com/oussama4/commentify/business/data/store"
 )
 
 type Page struct {
@@ -29,6 +30,15 @@ func (th *Page) routes() http.Handler {
 	return r
 }
 
+func (pi *PageInput) valid() error {
+	v := validate.New()
+
+	v.Check(pi.Title != "", "title", "page title is required")
+	v.Check(pi.Url != "", "url", "page url is required")
+
+	return v.Valid()
+}
+
 func (p *Page) List(w http.ResponseWriter, r *http.Request) {
 	qs := r.URL.Query()
 	page, _ := web.ReadInt(qs, "page", 0)
@@ -36,7 +46,7 @@ func (p *Page) List(w http.ResponseWriter, r *http.Request) {
 
 	pages, err := p.store.ListPages(page, pageSize)
 	if err != nil {
-		respondError(p.logger, w, http.StatusInternalServerError, err.Error())
+		respondError(p.logger, w, http.StatusInternalServerError, err)
 	}
 	web.Json(w, http.StatusOK, map[string]interface{}{"pages": pages})
 }
@@ -44,12 +54,16 @@ func (p *Page) List(w http.ResponseWriter, r *http.Request) {
 func (p *Page) Create(w http.ResponseWriter, r *http.Request) {
 	page := PageInput{}
 	if err := json.NewDecoder(r.Body).Decode(&page); err != nil {
-		respondError(p.logger, w, http.StatusInternalServerError, err.Error())
+		respondError(p.logger, w, http.StatusInternalServerError, err)
+	}
+
+	if err := page.valid(); err != nil {
+		respondError(p.logger, w, http.StatusUnprocessableEntity, err)
 	}
 
 	pageId, err := p.store.CreatePage(page.Url, page.Title)
 	if err != nil {
-		respondError(p.logger, w, http.StatusInternalServerError, err.Error())
+		respondError(p.logger, w, http.StatusInternalServerError, err)
 	}
 
 	web.Json(w, http.StatusOK, map[string]interface{}{"page_id": pageId})
@@ -60,9 +74,9 @@ func (p *Page) Get(w http.ResponseWriter, r *http.Request) {
 	page, err := p.store.GetPage(pageId)
 	if err != nil {
 		if errors.Is(err, store.ErrNotFound) {
-			respondError(p.logger, w, http.StatusNotFound, err.Error())
+			respondError(p.logger, w, http.StatusNotFound, err)
 		}
-		respondError(p.logger, w, http.StatusInternalServerError, err.Error())
+		respondError(p.logger, w, http.StatusInternalServerError, err)
 	}
 
 	web.Json(w, http.StatusOK, map[string]interface{}{"page": page})
