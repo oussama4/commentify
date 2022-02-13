ccd50b0ff2142e84ece18b9861c410e2e4453d05
osama zowaki <oussamazouaki4@gmail.com>
2022-02-13 20:14:22 +0100

    add database business package

---
+++ business/data/database/database.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"database/sql"
+	"net/url"
+
+	_ "github.com/lib/pq"
+)
+
+type Config struct {
+	User         string
+	Password     string
+	Host         string
+	Name         string
+	MaxIdleConns int
+	MaxOpenConns int
+	DisableTLS   bool
+}
+
+func Open(cfg Config) (*sql.DB, error) {
+	sslMode := "require"
+	if cfg.DisableTLS {
+		sslMode = "disable"
+	}
+
+	q := make(url.Values)
+	q.Set("sslmode", sslMode)
+	q.Set("timezone", "utc")
+
+	u := url.URL{
+		Scheme:   "postgres",
+		User:     url.UserPassword(cfg.User, cfg.Password),
+		Host:     cfg.Host,
+		Path:     cfg.Name,
+		RawQuery: q.Encode(),
+	}
+
+	db, err := sql.Open("postgres", u.String())
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxIdleConns(cfg.MaxIdleConns)
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+
+	return db, nil
+}
