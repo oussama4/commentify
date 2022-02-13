932decf8a4beb768ab3c5b21319e6bc52e9b6400
osama zowaki <oussamazouaki4@gmail.com>
2022-02-13 20:56:06 +0100

    add basic comment display component

---
+++ frontend/src/components/Comment.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Avatar from './atoms/Avatar.svelte'
+</script>
+
+<div class="flex flex-col">
+    <div class="flex flex-row justify-start">
+        <Avatar name="osama"/>
+        <div class="ml-2 text-xl"> Osama </div>
+    </div>
+    <div class="text-center text-lg">
+        content
+    </div>
+</div>
