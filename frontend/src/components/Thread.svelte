98f67cd90b339bef307424de3f04c9d10bca54fb
osama zowaki <oussamazouaki4@gmail.com>
2022-02-14 00:03:48 +0100

    add comment thread component

---
+++ frontend/src/components/Thread.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { Comment as CommentType } from "../interfaces/comment";
+    import Comment from './Comment.svelte';
+
+    export let comments = [];
+</script>
+
+<div class="flex flex-col">
+    <div>
+	    {#each comments as comment}
+            <Comment comment={comment}/>
+	    {/each}
+    </div>
+</div>
