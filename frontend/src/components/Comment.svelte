85a04814773987661f04eba2d4789ef3d90eb385
osama zowaki <oussamazouaki4@gmail.com>
2022-02-14 22:55:20 +0100

    use real data in comment component

--- frontend/src/components/Comment.svelte
+++ frontend/src/components/Comment.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
-    import Avatar from './atoms/Avatar.svelte'
+    import { Comment as CommentType } from "../interfaces/comment";
+    import Avatar from './atoms/Avatar.svelte';
+
+    export let comment: CommentType;
 </script>
 
-<div class="flex flex-col">
-    <div class="flex flex-row justify-start">
-        <Avatar name="osama"/>
-        <div class="ml-2 text-xl"> Osama </div>
+<div class="flex flex-row my-5">
+    <div class="basis-1/6">
+        <Avatar name={comment.author.name}/>
     </div>
-    <div class="text-center text-lg">
-        content
+    <div class="flex flex-col mx-2">
+        <div class="text-lg font-bold"> { comment.author.name } </div>
+        <div class="text-base">
+            { comment.body }
+        </div>
     </div>
 </div>
