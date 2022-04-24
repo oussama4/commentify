a27082debf1fece07fd669719358f92e5b9e74f3
osama zowaki <oussamazouaki4@gmail.com>
2022-04-24 00:39:25 +0000

    update nix go version

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/32356ce11b8cc5cc421b68138ae8c730cc8ad4a2.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/73ad5f9e147c0d2a2061f1d4bd91e05078dc0b58.tar.gz") {} }:
 
 pkgs.mkShell {
     name = "commentify-env";
     buildInputs = [
         pkgs.nodejs
-        pkgs.go_1_17
+        pkgs.go_1_18
         pkgs.yarn
         pkgs.go-task
     ];
